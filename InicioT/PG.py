8e79040f3843cc77b83a82af21c060ff0326a60b
RianLL15 <152223711+RianLL15@users.noreply.github.com>
2025-02-25 22:58:17 -0300

    Update PG.py

--- InicioT/PG.py
+++ InicioT/PG.py
@@ -1,8 +1,9 @@
 #############################################################################################################################
 
-#Sistema de input e indentificação da operação(simples):
+# Sistema de input e indentificação da operação(simples):
 
-# n1 = float(input("Escolha o primeiro número: "))
+
+# n1 = float(input("Escolha o primeiro número: ")) 
 # n2 = float(input("\nEscolha o segundo número: "))
 # op = str(input("\nEscolha a operação: "))
 
@@ -18,29 +19,114 @@
 
 #############################################################################################################################
 
-#Sistema de input e indentificação da operação(complexo):
+# Sistema de input e indentificação da operação(complexo):
 
-# n1 = float(input("Escolha o primeiro número: "))
-# n2 = float(input("\nEscolha o segundo número: "))
-# op = str(input("\nEscolha a operação: "))
-# r = int
-# Cop = str
+# import math
 
-# if op == "+": r = n1 + n2
-# elif op == "-": r = n1 - n2
-# elif op == "/": r = n1 / n2
-# elif op == "*": r = n1 * n2
-# elif op == "^": r = n1 ** n2
-# elif op == "%": r = n1 * (n2 / 100)
+# def Inicio():
+    
+#     opG = ["!", "log", "+", "-", "/", "*", "%", "^"]
 
-# if op == "+": Cop = " mais "
-# elif op == "-": Cop = " menos "
-# elif op == "/": Cop = " dividido "
-# elif op == "*": Cop = " multiplicado por "
-# elif op == "^": Cop = " elevado "
-# elif op == "%": Cop = " porcento de "
+#     while True:
 
-# print(f"\n{n1}{Cop}{n2} é igual a {r}")
+#         print("Temos essa opções de operação: !, log, +, -, /, *, %, ^")
+#         op = input("\nEscolha a operação: ")
+#         if op in opG:
+#             return op
+#         else:
+#             print("Operação inválida. Tente novamente.")        
+
+# def Fatorial():
+    
+#     n1 = int(input("\nEscolha um número: "))
+#     r = math.factorial(n1)
+#     Cop = "fatorial"
+#     print(f"O {Cop} de {n1} é igual a {r}")
+#     exit()
+
+# def Log():
+
+#     n1 = float(input("\nEscolha o primeiro número: "))
+#     n2 = float(input("\nEscolha o segundo número: "))
+#     r = math.log(n2, n1)
+#     print(f"Log na base {n1} e o logaritmando {n2} o X é igual a {r}")
+#     exit()
+
+# def Add():
+
+#     n1 = float(input("\nEscolha o primeiro número: "))
+#     n2 = float(input("\nEscolha o segundo número: "))
+#     r = n1 + n2
+#     Cop = " mais "
+#     print(f"{n1}{Cop}{n2} é igual a {r}")
+#     exit()
+
+# def Sub():
+
+#     n1 = float(input("\nEscolha o primeiro número: "))
+#     n2 = float(input("\nEscolha o segundo número: "))
+#     r = n1 - n2
+#     Cop = " menos "
+#     print(f"{n1}{Cop}{n2} é igual a {r}")
+#     exit()
+
+# def Mult():
+
+#     n1 = float(input("\nEscolha o primeiro número: "))
+#     n2 = float(input("\nEscolha o segundo número: "))
+#     r = n1 * n2
+#     Cop = " multiplaicado por "
+#     print(f"{n1}{Cop}{n2} é igual a {r}")
+#     exit()
+
+# def Div():
+
+#     n1 = float(input("\nEscolha o primeiro número: "))
+#     n2 = float(input("\nEscolha o segundo número: "))
+#     r = n1 / n2
+#     round(r, 2)
+#     Cop = " dividido "
+#     print(f"{n1}{Cop}{n2} é igual a {r}")
+#     exit()
+
+# def Elev():
+
+#     n1 = float(input("\nEscolha o primeiro número: "))
+#     n2 = float(input("\nEscolha o segundo número: "))
+#     r = n1 ** n2
+#     Cop = " elevado "
+#     print(f"{n1}{Cop}{n2} é igual a {r}")
+#     exit()
+
+# def Porc():
+
+#     n1 = float(input("\nEscolha o primeiro número: "))
+#     n2 = float(input("\nEscolha o segundo número: "))
+#     r = n1 * (n2 / 100)
+#     round(r, 2)
+#     Cop = " porcento de "
+#     print(f"{n1}{Cop}{n2} é igual a {r}")  
+#     exit()
+
+# while True:
+#     op = Inicio()
+
+#     if op == "+": 
+#         Add()
+#     elif op == "-": 
+#         Sub()
+#     elif op == "/": 
+#         Div()
+#     elif op == "*": 
+#         Mult() 
+#     elif op == "^": 
+#         Elev() 
+#     elif op == "!": 
+#         Fatorial()
+#     elif op == "log": 
+#         Log() 
+#     else: 
+#         Porc() 
 
 #############################################################################################################################
 
@@ -92,50 +178,105 @@
 #Adivinhe o número(Complexo):
 
 # import pygame
-# from pygame import mixer
 # import random
 # import time
 
 # pygame.init()
 
-# Erro = pygame.mixer.Sound('InicioT\\errou-rude.wav')
-# Win = pygame.mixer.Sound('InicioT\\ai-que-delicia-mickey.wav')
-# Decepcao = pygame.mixer.Sound('InicioT\\faz-o-l-luan-gameplays.wav')
+# Erro = pygame.mixer.Sound('Som\\P3IT\\errou-rude.wav')
+# Win = pygame.mixer.Sound('Som\\P3IT\\ai-que-delicia-mickey.wav')
+# Decepcao = pygame.mixer.Sound('Som\\P3IT\\faz-o-l-luan-gameplays.wav')
+# Special = pygame.mixer.Sound('Som\\P3IT\\ai-meu-c-zinho-w-penelope-di-monaco.wav')
 
 # nA = random.randint(1, 100)
-# T = 0
-# TM = 3
-# t = 0
-# tm = 600
 
-# while T < TM:
-#     T += 1
-
-#     P = int(input("Advinhe um número de 1 a 100: "))
-
-#     if P == nA: 
-#         print("\nVocê acertou!!!!")
-#         Win.play()
-#         time.sleep(5)
-#         exit()
-#     elif T == TM: 
-#         print(f"\nA resposta correta era {nA}")
-#         while t < tm:
-#             Decepcao.play()
-#             time.sleep(3)
+# def Inicio():
         
-#         tm -= 1
-#         exit()
-#     elif  P > nA: 
-#         print("\nVocê está no caminho, tente um número menor")
-#         Erro.play()
-#         time.sleep(2)
-#         pygame.mixer.music.unpause()
-#     elif P < nA:
-#         print("\nVocê está no caminho, tente um número maior")
-#         Erro.play()
-#         time.sleep(2)
-#         pygame.mixer.music.unpause() 
+#         print("Dificuldades disponíveis: f(Fácil) ou d(Difícl)")
+#         Df = input("Escolha a dificuldade: ").lower()
+
+#         while Df != "f" and Df != "d":
+#             print("\nDificuldades disponíveis: f(Fácil) ou d(Difícl)")
+#             Df = input("Escolha a dificuldade: ").lower()
+
+#         return Df
+        
+# def DfD(): 
+
+#         T = 0 #Tentativas
+#         TM = 90 #Tentativas Max
+#         t = 0 #Tempo
+#         tm = 600 #Tempo Max
+#         print(nA)
+
+#         while T < TM:
+
+#             T += 1
+
+#             P = int(input("\nAdvinhe um número de 1 a 100: "))
+
+#             if P == nA: 
+#                 print("\nVocê acertou!!!!")
+#                 while t < tm:
+#                     Special.play()
+#                     time.sleep(2)
+#                 tm -= 1
+#                 exit()
+#             elif T == TM: 
+#                 print(f"\nA resposta correta era {nA}")
+#                 while t < tm:
+#                      Decepcao.play()
+#                      time.sleep(3)
+#                 tm -= 1
+#                 exit()
+#             elif  P > nA: 
+#                 print("\nVocê está no caminho, tente um número menor")
+#                 Erro.play()
+#                 time.sleep(2)
+#                 pygame.mixer.music.unpause()
+#             else:
+#                 print("\nVocê está no caminho, tente um número maior")
+#                 Erro.play()
+#                 time.sleep(2)
+#                 pygame.mixer.music.unpause()
+
+# def DfF():
+        
+#         T = 0 #Tentativas
+#         t = 0 #Tempo
+#         tm = 600 #Tempo Max
+
+#         while True:
+
+#             T += 1
+
+#             P = int(input("\nAdvinhe um número de 1 a 100: "))
+
+#             if P == nA: 
+#                 print(f"\nVocê acertou o numero em {T} tentativa(s)!!!!")
+#                 while t < tm:
+#                     Win.play()
+#                     time.sleep(1)
+#                 tm -= 1
+#                 exit()
+#             elif  P > nA: 
+#                 print("\nVocê está no caminho, tente um número menor")
+#                 Erro.play()
+#                 time.sleep(2)
+#                 pygame.mixer.music.unpause()
+
+#             else:
+#                 print("\nVocê está no caminho, tente um número maior")
+#                 Erro.play()
+#                 time.sleep(2)
+#                 pygame.mixer.music.unpause()
+
+# Df = Inicio()
+
+# if Df == "d": 
+#     DfD() 
+# else: 
+#     DfF() 
 
 #######################################################################################################################################
 
